4ce5c633d7c148a50597dd2e4a7ffbb9bd9aaff9
juliiaburman <buju23nb@student.ju.se>
2026-03-04 12:25:30 +0100

    displaying the venue cards

--- public/client.js
+++ public/client.js
@@ -1,11 +1,33 @@
-
 // Retrieves the venue data from the server, converting it into JSON
 function fetchVenues() {
-    fetch("/venues")
-  .then((response) => response.json())
-  .then(data => {
-    console.log(data)
-  });
+  fetch("/api/venues")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("---------> DATA:", data);
+      data.forEach((element) => {
+        let card = document.createElement("div");
+        card.innerHTML = `
+<div class="venue-card">
+<div class="venue-image">
+      <img src="${element.image_url}" alt="Cafe placeholder image" />
+    </div>
+
+    <div class="venue-name">
+      <h3>${element.name}</h3>
+    </div>
+
+    <div class="venue-district">${element.district}</div>
+    <div class="venue-type">${element.category}</div>
+
+    <div class="venuecard-buttons">
+      <button class="edit-button">Edit</button>
+      <button class="delete-button">Delete</button>
+    </div></div>`;
+        document.getElementById("listofshops").appendChild(card);
+      });
+    });
 }
 
-fetchVenues();
+document.addEventListener("DOMContentLoaded", () => {
+  fetchVenues();
+});
