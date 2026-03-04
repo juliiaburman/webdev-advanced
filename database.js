94b025ffaf23abaa916183228c6e4261736fa155
juliiaburman <buju23nb@student.ju.se>
2026-03-04 09:58:36 +0100

    correct json file

--- database.js
+++ database.js
@@ -1,3 +1,5 @@
+//This code should only be run once to add the stores.json into the database
+
 // Set up a database client to allow Node to connect to the PostgreSQL database running in the Docker container
 const { Client } = require("pg");
 // Configure the client to connect to your containerized PostgreSQL
@@ -18,4 +20,24 @@ async function connectDB() {
     console.error("Connection error", err.stack);
   }
 }
+// Disconnect to ensure that all the data is in the database
+async function disconnectDB() {
+  try {
+    await client.end();
+    console.log("Disconnected to PostgreSQL database with async/await");
+  } catch (err) {
+    console.error("Disconnection error", err.stack);
+  }
+}
+
 connectDB(); // should be called before any other function
+
+let data = readfile("stores.json");
+let id = 1;
+data.forEach((s) => {
+  const res = client.query(query, [id, s.name, s.url, s.district]);
+  console.log(res.rows[0]);
+  id = id + 1;
+});
+
+disconnectDB(); //should be called at the end
--- server.js
+++ server.js
@@ -1,8 +1,29 @@
 //Part of the server side (Node + Express)
 
+const { Client } = require("pg"); // Set up a database client to allow Node to connect to the PostgreSQL database running in the Docker container
 const express = require("express"); //loads the Express library so it's installed
 const app = express(); //creates your server application
 const PORT = 3000; //port number where the server will run
+
+const client = new Client({ // Configure the client to connect to your containerized PostgreSQL
+  host: "localhost", // since the container's port is mapped to localho
+  port: 5432,
+  user: "postgres", // default user
+  password: "12345", // password set in the container command
+  database: "postgres", // default database
+});
+
+// Establish the connection so the PostgreSQL database can be used in the Node application
+async function connectDB() {
+  try {
+    await client.connect();
+    console.log("Connected to PostgreSQL database with async/await");
+  } catch (err) {
+    console.error("Connection error", err.stack);
+  }
+}
+connectDB(); // should be called before any other function
+
 
 // Middlewares
 app.use(express.json()); //allows us to parse/convert incoming JSON request bodies to JavaScript object so that Express can read it
